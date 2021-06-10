61edfa1e1c16be5c99792cf0fa0dd09cb3066515
Andrey Shchepkin <lovenfed@gmail.com>
2021-06-10 10:54:37 +0300

    american express/FAIL

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "6011710856846264";
+        String number = "343300613774496";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
