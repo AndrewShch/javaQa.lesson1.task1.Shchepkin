b1363ddb10db1c2f3188cbbda9fcce29c9b3f31e
Andrey Shchepkin <lovenfed@gmail.com>
2021-06-10 11:04:07 +0300

    test2 discover/OK

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "5367645028477428";
+        String number = "6011164342779046";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
