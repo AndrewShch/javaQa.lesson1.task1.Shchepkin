1174377427fc7dc3fc7e8f8d41b962365d00c0e4
Andrey Shchepkin <lovenfed@gmail.com>
2021-06-10 11:01:12 +0300

    test2 visa/OK

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "343300613774496";
+        String number = "4024007152741780";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
